d996488ce99850f64f7278794037dd049846b419
Michael Wisely <michaelwisely@gmail.com>
2013-02-16 19:11:43 -0600

    Did a bit of refactoring, styling, and created views for branches and tags
    Also got diffs to display for a commit. Yay!

--- src/greta/urls.py
+++ src/greta/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
-from .views import RepositoryList, RepositoryDetail, CommitDetail
+from .views import (RepositoryList, RedirectToDefaultBranch, RepositoryDetail,
+                    CommitDetail, BranchDetail, TagDetail)
 
 
 urlpatterns = patterns(
@@ -8,6 +9,15 @@ urlpatterns = patterns(
     url(r'^$',
         RepositoryList.as_view(),
         name='repo_list'),
+    url(r'^(?P<pk>\d+)/$',
+        RedirectToDefaultBranch.as_view(),
+        name='repo_detail'),
+    url(r'^(?P<pk>\d+)/branches/$',
+        BranchDetail.as_view(),
+        name='branch_detail'),
+    url(r'^(?P<pk>\d+)/tags/$',
+        TagDetail.as_view(),
+        name='tag_detail'),
     # url(r'^(?P<pk>\d+)/(?P<ref>.+)/tree/(?P<path>.+)/$',
     #     RepositoryDetail.as_view(),
     #     name='tree_detail'),
--- src/greta/views.py
+++ src/greta/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import RedirectView, ListView, DetailView
+from django.shortcuts import get_object_or_404
 
 from .models import Repository
 
@@ -7,23 +8,53 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+
+class GretaContextMixin(object):
+    def get_context_data(self, **kwargs):
+        context = super(GretaContextMixin, self).get_context_data(**kwargs)
+        context['ref'] = self.kwargs['ref']
+        return context
+
+
 class RepositoryList(ListView):
     model = Repository
     context_object_name = "repos"
     template_name = "greta/repository_list.html"
 
 
-class RepositoryDetail(DetailView):
+class RedirectToDefaultBranch(RedirectView):
+    def get_redirect_url(self, **kwargs):
+        repo = get_object_or_404(Repository, pk=self.kwargs['pk'])
+        return repo.get_absolute_url()
+
+
+class BranchDetail(DetailView):
+    model = Repository
+    context_object_name = "repo"
+    template_name = "greta/branch_detail.html"
+
+
+class TagDetail(DetailView):
+    model = Repository
+    context_object_name = "repo"
+    template_name = "greta/tag_detail.html"
+
+
+class RepositoryDetail(GretaContextMixin, DetailView):
     model = Repository
     context_object_name = "repo"
     template_name = "greta/repository_detail.html"
 
+    def get_context_data(self, **kwargs):
+        context = super(RepositoryDetail, self).get_context_data(**kwargs)
+        context['log'] = self.object.get_log(ref=self.kwargs['ref'])
+        return context
 
-class CommitDetail(DetailView):
-    model = Repository
-    context_object_name = "repo"
+
+class CommitDetail(RepositoryDetail):
     template_name = "greta/commit_detail.html"
 
     def get_context_data(self, **kwargs):
         context = super(CommitDetail, self).get_context_data(**kwargs)
+        context['changes'] = self.object.show(self.kwargs['ref'])
         return context
