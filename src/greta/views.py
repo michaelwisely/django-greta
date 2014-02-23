c416ff5f536daa7cdb6059c57ca8471febb1bbd3
Michael Wisely <michaelwisely@gmail.com>
2014-02-23 13:13:09 -0600

    Refactor CommitDetail
    Refactor some functionality and add "branches" to the context so that
    we can list the branches that point to a commit.

--- src/greta/views.py
+++ src/greta/views.py
@@ -131,25 +131,45 @@ class RepositoryDetail(GretaMixin, DetailView):
 class CommitDetail(GretaMixin, DetailView):
     template_name = "greta/commit_detail.html"
 
+    def find_tags(self, commit, repo):
+        """
+        Find tags that point to this commit
+        ref_dict maps tag names ('refs/tags/...') to sha's
+        """
+        ref_dict = repo.repo.refs.as_dict()
+        tags = []
+        for tag, tag_id in [(t, ref_dict[t]) for t in repo.tags]:
+            obj, obj_id = repo.repo[tag_id], None
+            if isinstance(obj, Tag):
+                _, obj_id = obj.object
+            if isinstance(obj, Commit):
+                obj_id = obj.id
+            if commit.id == obj_id:
+                tags.append((tag, obj))
+        return tags
+
+    def find_branches(self, commit, repo):
+        """
+        Find branches that point to this commit
+        ref_dict maps tag names ('refs/tags/...') to sha's
+        """
+        ref_dict = repo.repo.refs.as_dict()
+        branches = []
+        for branch, branch_id in [(b, ref_dict[b]) for b in repo.branches]:
+            obj = repo.repo[branch_id]
+            if commit.id == obj.id:
+                branches.append((branch, obj))
+        return branches
+
     def get_context_data(self, **kwargs):
         context = super(CommitDetail, self).get_context_data(**kwargs)
         try:
             repo = self.object
             context['changes'] = repo.show(self.full_ref)
             context['commit'] = repo.get_commit(self.full_ref)
+            context['tags'] = self.find_tags(context['commit'], repo)
+            context['branches'] = self.find_branches(context['commit'], repo)
 
-            # Find tags that point to this commit
-            # tag_dict maps tag names ('refs/tags/...') to sha's
-            tag_dict = repo.repo.refs.as_dict()
-            context['tags'] = []
-            for tag, tag_id in [(t, tag_dict[t]) for t in repo.tags]:
-                obj, obj_id = repo.repo[tag_id], None
-                if isinstance(obj, Tag):
-                    _, obj_id = obj.object
-                if isinstance(obj, Commit):
-                    obj_id = obj.id
-                if context['commit'].id == obj_id:
-                    context['tags'].append((tag, obj))
         except KeyError:
             raise Http404("Bad ref")
         return context
